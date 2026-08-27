2b9bebf8d0eca0b5ea30b859a1f393a39ac2893c
doyeon <doyun20445@gmail.com>
2026-08-27 14:33:51 +0900

    feat: 예매 상세 내역 조회 시 결제 정보 포함하도록 추가

--- src/main/java/com/ohgiraffers/airquery/reservation/model/service/ReservationService.java
+++ src/main/java/com/ohgiraffers/airquery/reservation/model/service/ReservationService.java
@@ -1,7 +1,10 @@
 package com.ohgiraffers.airquery.reservation.model.service;
 
+import com.ohgiraffers.airquery.payment.model.dao.PaymentDAO;
+import com.ohgiraffers.airquery.payment.model.dto.PaymentDTO;
 import com.ohgiraffers.airquery.reservation.model.dao.ReservationDAO;
 import com.ohgiraffers.airquery.reservation.model.dto.ReservationDTO;
+import com.ohgiraffers.airquery.reservation.model.dto.ReservationDetailDTO;
 
 import java.sql.Connection;
 import java.util.List;
@@ -11,8 +14,10 @@ import static com.ohgiraffers.airquery.common.JDBCTemplate.getConnection;
 
 public class ReservationService {
 
-    private ReservationDAO reservationDAO = new ReservationDAO();
+    private final ReservationDAO reservationDAO = new ReservationDAO();
+    private final PaymentDAO paymentDAO = new PaymentDAO();
 
+    /* 전체 예매 목록 조회 */
     public List<ReservationDTO> selectAllReservations(int memberCode) {
 
         Connection con = getConnection();
@@ -24,14 +29,23 @@ public class ReservationService {
         return reservationList;
     }
 
-    public ReservationDTO findById(int reservationCode, int memberCode) {
+    /* 예매 상세 내역 조회 */
+    public ReservationDetailDTO findReservationDetail(int reservationCode, int memberCode) {
 
         Connection con = getConnection();
 
+        ReservationDetailDTO detail = null;
+
+        // 예매(티켓), 결제, 수하물, 좌석
         ReservationDTO reservation = reservationDAO.findById(con, reservationCode, memberCode);
+        PaymentDTO payment = paymentDAO.findByReservation(con, reservationCode);
 
         close(con);
 
-        return reservation;
+        detail = ReservationDetailDTO.of(reservation, payment);
+
+        return detail;
     }
+
+
 }
