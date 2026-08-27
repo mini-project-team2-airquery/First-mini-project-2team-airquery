1ba9654e2f2667542febae15ac63ae95d2a570be
doyeon <doyun20445@gmail.com>
2026-08-27 11:14:26 +0900

    feat: 예매 목록 조회, 예매 상세 조회 기본 동작 구현

---
+++ src/main/java/com/ohgiraffers/airquery/reservation/controller/ReservationController.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.airquery.reservation.controller;
+
+import com.ohgiraffers.airquery.reservation.model.dao.ReservationDAO;
+import com.ohgiraffers.airquery.reservation.model.dto.ReservationDTO;
+import com.ohgiraffers.airquery.reservation.model.service.ReservationService;
+
+import java.util.List;
+
+public class ReservationController {
+
+    private final ReservationService reservationService = new ReservationService();
+
+    public List<ReservationDTO> getAllReservations(int memberCode) {
+
+        return reservationService.selectAllReservations(memberCode);
+    }
+
+    public ReservationDTO getReservationDetail(int reservationCode, int memberCode) {
+
+        return reservationService.findById(reservationCode, memberCode);
+    }
+}
