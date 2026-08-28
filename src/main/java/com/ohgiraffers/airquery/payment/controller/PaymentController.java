5a5b166cf98d23c63ab9fbf66e449fbf3c421353
doyeon <doyun20445@gmail.com>
2026-08-28 12:09:05 +0900

    feat: 결제 등록 구현

---
+++ src/main/java/com/ohgiraffers/airquery/payment/controller/PaymentController.java
@@ -0,0 +1,38 @@
+package com.ohgiraffers.airquery.payment.controller;
+
+import com.ohgiraffers.airquery.payment.model.dto.PaymentDTO;
+import com.ohgiraffers.airquery.payment.model.service.PaymentService;
+
+import java.util.Map;
+
+public class PaymentController {
+
+    private final PaymentService paymentService = new PaymentService();
+
+    /* 결제 등록 */
+    public int registerPayment(Map<String, Object> requestMap) {
+
+        PaymentDTO paymentDTO = new PaymentDTO();
+
+        paymentDTO.setReservationCode((int) requestMap.get("reservation_code"));
+        paymentDTO.setPaymentAmount((int) requestMap.get("payment_amount"));
+        paymentDTO.setPaymentMethod((String) requestMap.get("payment_method"));
+        paymentDTO.setRefundStatus((boolean) requestMap.get("refund_status"));
+
+        int result = paymentService.registerPayment(paymentDTO);
+
+        if(result == 1) {
+            System.out.println("결제 성공");
+        } else {
+            System.out.println("결제 실패");
+        }
+
+        return result;
+    }
+
+    /* 전체 결제할 금액 조회 */
+    public int getTotalPaymentAmount(int reservationCode) {
+
+        return paymentService.getTotalPaymentAmount(reservationCode);
+    }
+}
