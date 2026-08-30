1f9388931f4454c2be22dcda8ce2636f708ef26b
doyeon <doyun20445@gmail.com>
2026-08-30 11:07:12 +0900

    fix: 결제, 예매 화면에 회원만 접근하도록 수정

--- src/main/java/com/ohgiraffers/airquery/Application.java
+++ src/main/java/com/ohgiraffers/airquery/Application.java
@@ -205,7 +205,7 @@ public class Application {
 
         if("Admin".equals(loginMember.getMemberAuth())){
 
-            reservationMenu.displayMenu(sc);
+            //reservationMenu.displayMenu(sc);
         } else if("Member".equals(loginMember.getMemberAuth())){
 
             reservationMenu.displayMenu(sc);
@@ -223,9 +223,10 @@ public class Application {
 
         if("Admin".equals(loginMember.getMemberAuth())){
 
-            paymentMenu.displayMenu(sc);
+            //paymentMenu.displayMenu(sc);
         } else if("Member".equals(loginMember.getMemberAuth())){
 
+            paymentMenu.displayMenu(sc);
         } else {
             System.out.println("알수없는 회원입니다.");
         }
